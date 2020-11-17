5774d9d820ec946dfddc43449053c9ccdbd4da13
VianTio <59288339+VianTio@users.noreply.github.com>
2020-11-17 11:06:24 +0700

    Add files via upload

--- PHP ( Fungsi dan Array )/Tugas_Array.php
+++ PHP ( Fungsi dan Array )/Tugas_Array.php
@@ -12,15 +12,16 @@ $matrik2 = [
 ];
 
 $hasil = [
-    []
+    [],
 ];
 
-for ($i = 0; $i <= sizeof($matrik1[0]) - 1; $i++) {
-    for ($j = 0; $j <= sizeof($matrik1) - 1; $j++) {
+for ($i = 0; $i <= sizeof($matrik1) - 1; $i++) {
+    for ($j = 0; $j <= sizeof($matrik1[0]) - 1; $j++) {
         $hasil[$i][$j] = $matrik1[$i][$j] + $matrik2[$i][$j];
     }
 }
 
-echo '<pre>';
+echo "<pre>";
 print_r($hasil);
-echo '</pre>';
+echo "</pre>";
+?>
